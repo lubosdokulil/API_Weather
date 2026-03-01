2fc48f29ac80dbe6b9cd5f902fb65f097b0c1694
lubosdokulil <lubosdokulil6@gmail.com>
2026-03-01 13:37:33 +0100

    Přidejte soubory projektu.

---
+++ API_Weather/Models/CurrentWeather.cs
@@ -0,0 +1,9 @@
+﻿namespace API_Weather.Models
+{
+    public class CurrentWeather
+    {
+        public double temperature { get; set; }
+        public double windspeed { get; set; }
+        public string time { get; set; }
+    }
+}
---
+++ API_Weather/Models/WeatherResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace API_Weather.Models
+{
+    public class WeatherResponse
+    {
+        // Tato vlastnost se musí jmenovat přesně jako v JSONu
+        public CurrentWeather current_weather { get; set; }
+    }
+}
